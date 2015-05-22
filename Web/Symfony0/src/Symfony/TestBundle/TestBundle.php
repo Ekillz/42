458c62398faf6a1315e95eb67efe29626b6fb68d
Emil MAMMADZADA <emammadz@e2r8p5.42.fr>
2015-05-22 17:32:03 +0200

    deleted wrong symfony-

--- Web/Symfony0/src/Symfony/TestBundle/TestBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Symfony\TestBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TestBundle extends Bundle
-{
-}
--- Web/Symfony0/src/Symfony/UserBundle/SymfonyUserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Symfony\UserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SymfonyUserBundle extends Bundle
-{
-}
